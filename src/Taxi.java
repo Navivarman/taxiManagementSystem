b67caba37949fb573445179af3c3fb3e52ac7ce6
Navivarman <navi200511@gmail.com>
2025-11-12 19:16:19 +0530

    Taxi class

---
+++ src/Taxi.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Taxi {
+    private  int id;
+    private char cSpot;
+    private int freeTime;
+    private int earnings;
+    private List<Booking> booking;
+
+    Taxi(int id,char cSpot,int freeTime,int earnings,Object booking){
+        this.id = id;
+        this.cSpot = cSpot;
+        this.freeTime = freeTime;
+        this.earnings = earnings,
+        this.booking = new ArrayList<>();
+    }
+}
